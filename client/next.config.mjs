ee0183d11113418ce450380b0235bbb6db567111
samir-sah <sahsamir2004@gmail.com>
2026-08-27 23:24:02 +0530

    initialized API configuration and proxy rewrites

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4400'}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
